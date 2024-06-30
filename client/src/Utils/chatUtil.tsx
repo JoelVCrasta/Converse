20816ad75f7b10710c628ddcf69f99679ccbc253
Joel Crasta <loejstarc@gmail.com>
2024-06-30 05:56:17 +0530

    fixed chat name util

--- client/src/Utils/chatUtil.tsx
+++ client/src/Utils/chatUtil.tsx
@@ -2,6 +2,6 @@ import { User } from "../Types/types"
 
 export function getEndUser(logged: User, users: User[]): string {
   console.log(logged)
-  let u = users[0]._id === logged._id ? users[0].name : users[1].name
+  let u = users[0]._id === logged._id ? users[1].name : users[0].name
   return u
 }
